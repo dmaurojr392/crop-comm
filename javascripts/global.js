01633e14cab9021893f68871242d86bfaa284fb6
dondonbridgeee <dmaurojr392@gmail.com>
2025-02-03 17:06:25 +0800

    re-route

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,3 +1,5 @@
 document.querySelectorAll('.appName').forEach(element => {
     element.innerText = "AgriMetrics";
 });
+document.getElementsByClassName('menu-title')[0].innerText = "Dashboard";
+document.getElementsByClassName('menu-title')[1].innerText = "Crop Map";
