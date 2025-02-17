8ec3c2e46a34323a581ff9882dfdae70d08763c0
dmaurojr392 <dmaurojr392@gmail.com>
2025-02-17 17:34:40 +0800

    made a few changes

--- javascripts/global.js
+++ javascripts/global.js
@@ -2,8 +2,3 @@ document.documentElement.style.setProperty('--animate-duration', '.75s');
 document.querySelectorAll('.appName').forEach(element => {
     element.innerText = "AgriMetrics";
 });
-document.getElementsByClassName('menu-title')[0].innerText = "Dashboard";
-document.getElementsByClassName('menu-title')[1].innerText = "Crop Map";
-
-document.getElementsByClassName('nav-link')[0].innerText = "Dashboard";
-document.getElementsByClassName('nav-link')[1].innerText = "Crop Map";
---
+++ javascripts/script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('menu-title')[0].innerText = "Dashboard";
+document.getElementsByClassName('menu-title')[1].innerText = "Crop Map";
+
+document.getElementsByClassName('nav-link')[0].innerText = "Dashboard";
+document.getElementsByClassName('nav-link')[1].innerText = "Crop Map";
